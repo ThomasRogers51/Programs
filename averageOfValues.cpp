a123ce2ca896bcc209acd6d16f2a79a9f9fa62da
Thomas Rogers <58536597+ThomasRogers51@users.noreply.github.com>
2020-01-16 23:14:34 -0500

    Create averageOfValues.cpp

---
+++ averageOfValues.cpp
@@ -0,0 +1,32 @@
+/*
+
+To get the average of a series of values, you add the values up and then divide the sum by the number of values. Write a program
+that stores the following values in five different variables: 28, 32, 37, 24, and 33. The program should first calculate the sum
+of these five variables and store the result in a separate variable named sum. Then, the program should divide the sum variable by
+5 to get the average. Display the average on the screen.
+
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int a = 28,
+		b = 32,
+		c = 37,
+		d = 24,
+		e = 33;
+
+	double sum = a + b + c + d + e;
+
+	double average = sum / 5;
+
+	cout << "The values are " << a <<", " << b << ", " << c << ", " << d << ", " << e << "." << endl;
+	cout << "The sum of the values is " << sum << "." << endl;
+	cout << "The average of the values is " << setprecision(1) << fixed << average << "." << endl;
+
+	return 0;
+}
